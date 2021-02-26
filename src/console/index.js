9901d343ec812c8f060f756426e2cd8b09b6a293
may-h <hms9210@naver.com>
2021-02-26 17:45:40 +0900

    output format 수정

--- src/console/index.js
+++ src/console/index.js
@@ -7,11 +7,11 @@ module.exports = async (inspection_result) => {
     //터미널 콘솔 출력. 
     for (category of Object.keys(inspection_result)) {
         const commands = inspection_result[category];
-        console.log(chalk.blue(`    [${category}]`));
+        console.log(chalk.blue(`[${category}]`));
         for (command of commands) {
-            console.log(chalk.yellow(`      ├ 항목 : ${command.name}`));
-            console.log(chalk.yellow(`      ├ 명령어 : ${command.command}`));
-            console.log(chalk.yellow(`      └ 출력내용 : ${command.response}\n`));
+            console.log(chalk.yellow(`├ 항목 : ${command.name}`));
+            console.log(chalk.yellow(`├ 명령어 : ${command.command}`));
+            console.log(chalk.yellow(`└ 출력내용 : ${command.response}\n`));
         }
       };
 };
