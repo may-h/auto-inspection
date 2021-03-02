569d42baeca4f6b2f25fd56cf4d791cc74295b16
may-h <hms9210@naver.com>
2021-03-02 10:05:03 +0900

    저장경로 수정

--- src/json/index.js
+++ src/json/index.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
 const utils = require('../../utils');
+const approot = require('app-root-path').path;
 
 //JSON 파일 저장. 
 module.exports = async (inspection_result) => {
     try {
         const filename = await utils.getFilename();
-        fs.writeFileSync(`${__dirname}/../../output/${filename}.json`, JSON.stringify(inspection_result, null, 4), 'utf8');
+        fs.writeFileSync(`${approot}/output/${filename}.json`, JSON.stringify(inspection_result, null, 4), 'utf8');
     } catch(err) {
         console.log('ERROR -> ', err)
     }
